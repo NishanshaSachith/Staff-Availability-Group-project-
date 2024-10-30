808ba171c7e48cab70644b9b263a7dce4c5e133d
Nishansha Sachith Dilushan <151370609+NishanshaSachith@users.noreply.github.com>
2024-10-30 10:05:16 +0530

    Update delete_schedule.php

--- page/delete_schedule.php
+++ page/delete_schedule.php
@@ -5,7 +5,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "staff_availability_system";
+$database = "group6";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
