d6921a1c5b0f92bbca1312d0c2b465bbae13727b
Nishansha Sachith Dilushan <151370609+NishanshaSachith@users.noreply.github.com>
2024-11-08 14:43:40 +0530

    Update AppointmentManagement.php

--- page/AppointmentManagement.php
+++ page/AppointmentManagement.php
@@ -217,7 +217,7 @@ $conn->close();
     </section>
 
     <footer>
-        <p>&copy; 2024 Computer Science Department. All rights reserved.</p>
+        <p>&copy; 2024 Department Of Computer Science. All rights reserved.</p>
     </footer>
 
     <script>
